bd673d5732b62c06c9133a9148017c0daef960fb
Charb G. <charb@Charbs-MacBook-Pro.local>
2026-02-17 12:39:23 -0800

    improve entry script

--- com-utils.py
+++ com-utils.py
@@ -40,7 +40,6 @@ def print_descendants(element):
         except Exception:
             pass
 
-
 def get_main_window() -> HwndWrapper:
     window = find_element_by_class_name("CSIUTW2025")
     p1 = find_element_by_auto_id("59648", window)
@@ -48,85 +47,94 @@ def get_main_window() -> HwndWrapper:
     p2.SetFocus()
     return HwndWrapper(p2.CurrentNativeWindowHandle)
 
-
-## Sandbox for testing
-
-TAB_COUNT = 30
-
-entry = {
-    "20000": "S",
-    "20001": "123456789",
-    "20002": "ACME INC",
-    "20005": "1",
-    "20006": "S",
-    "20011": "X",
-    "20012": "X",
-    "20013": "X",
-    "20014": "2",
-    "20017": "X",
-    "20018": "123ABC",
-    "20019": "99956452",
-    "20020": "99956453",
-    "20021": "99956454",
-    "20022": "99956455",
-    "20023": "123 Main St.",
-    "20024": "Houston",
-    "20025": "TX",
-    "20026": "77002",
-    "20027": "375000",
-    "20028": "55000",
-    "20029": "75000",
-    "20030": "64000",
-    "20031": "100000",
-    "20033": "579",
-    "20034": "11000",
-    "20035": "12000",
-    "20036": "13000",
-    "20037": "67 Long-Bridge Ave.",
-    "20038": "New York",
-    "20039": "NY",
-    "20040": "10007",
-    "20044": "14000",
-    "20045": "15000",
-    "20046": "X",
-    "20047": "X",
-    "20048": "X",
-    "20049": "1231",
-    "20050": "104",
-    "20052": "TX",
-    "20055": "15000",
-    "20056": "16000",
-    "20057": "17000",
-    "20058": "AS",
-    "20059": "19000"
-}
-
-window = get_main_window()
-
-for i in range(TAB_COUNT):
-    window.set_focus()
-    window.type_keys("{TAB}")
+def tab_into_next_field(w: HwndWrapper):
+    w.set_focus()
+    w.type_keys("{TAB}")
     time.sleep(0.3)
-    try:
-        element = uia.get_focused_element()
-        el = HwndWrapper(element.CurrentNativeWindowHandle)
-        key = str(el.element_info.control_id)
-        if key in entry:
-            value = entry[key]
-            el.type_keys(value)
-        else:
-            print(f"did not find key: {key} in entry - trying parent")
-            parent = el.parent()
-            if parent is not None:
-                print(f"found parent {parent.element_info.control_id}")
-                key = str(parent.element_info.control_id)
-                if key in entry:
-                    value = entry[key]
-                    parent.type_keys(value)
-                else:
-                    print(f"did not find key: {key} in entry - skipping")
-            else:
-                print("parent not found - skipping")
 
-    except Exception as e:
-        print(f"ran into an error for field: {key} value: {value} error: {e}")
+
+def enter_data(entry: dict, tab_count: int = 50):
+    def on_successful_entry(key):
+        del entry[key]
+        print(f"successfully entered: {key}, remaining: {len(entry)}")
+    
+    window = get_main_window()
+
+    for i in range(tab_count):
+        try:
+            element = uia.get_focused_element()
+            el = HwndWrapper(element.CurrentNativeWindowHandle)
+            key = str(el.element_info.control_id)
+            if key in entry:
+                value = entry[key]
+                el.type_keys(value)
+                on_successful_entry(key)
+            else:
+                parent = el.parent()
+                if parent is not None:
+                    key = str(parent.element_info.control_id)
+                    if key in entry:
+                        value = entry[key]
+                        parent.type_keys(value)
+                        on_successful_entry(key)
+            if len(entry) == 0:
+                print("all entries have been entered - stopping")
+                break
+            tab_into_next_field(window)
+
+        except Exception as e:
+            print(f"ran into an error for field: {key} value: {value} error: {e}")
+
+
+def main():
+    ## Sandbox for testing
+    enter_data({
+        "20000": "S",
+        "20001": "123456789",
+        "20002": "ACME INC",
+        "20005": "1",
+        "20006": "S",
+        "20011": "X",
+        "20012": "X",
+        "20013": "X",
+        "20014": "2",
+        "20017": "X",
+        "20018": "123ABC",
+        "20019": "99956452",
+        "20020": "99956453",
+        "20021": "99956454",
+        "20022": "99956455",
+        "20023": "123 Main St.",
+        "20024": "Houston",
+        "20025": "TX",
+        "20026": "77002",
+        "20027": "375000",
+        "20028": "55000",
+        "20029": "75000",
+        "20030": "64000",
+        "20031": "100000",
+        "20033": "579",
+        "20034": "11000",
+        "20035": "12000",
+        "20036": "13000",
+        "20037": "67 Long-Bridge Ave.",
+        "20038": "New York",
+        "20039": "NY",
+        "20040": "10007",
+        "20044": "14000",
+        "20045": "15000",
+        "20046": "X",
+        "20047": "X",
+        "20048": "X",
+        "20049": "1231",
+        "20050": "104",
+        "20052": "TX",
+        "20055": "15000",
+        "20056": "16000",
+        "20057": "17000",
+        "20058": "AS",
+        "20059": "19000"
+    })
+
+if __name__ == "__main__":
+    main()
